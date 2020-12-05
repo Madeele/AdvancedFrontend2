df90b031b58b14011ba9e806b65011a06505b433
Maddy <eaammpo@students.eaaa.dk>
2020-12-05 13:30:41 +0100

    Map code is back

--- js/map.js
+++ js/map.js
@@ -1,15 +1,57 @@
 
 
-    function template() {
-        document.querySelector("#app").innerHTML += /*html*/ `
-            
-        <section id="map" class="page">
-
-           <div>
+function mapTemplate() {
+    document.querySelector("#app").innerHTML += /*html*/ `    
+        <section id="map" class="page map_page">
+            <header class="topbar">
                <h2>Kunste Museum Map</h2>
+            </header>
+            <div class="nav-btn">
+                    <div></div>
+                    <div></div>
+            </div>
+            <div class="map_container">
+                <div class="map_floors">
+                    <div class="circles_numbers">
+                    <p>Floor</p>
+                        <div class="circle">-3</div>
+                    </div>
+                    <div class="circles_numbers">
+                    <p>Floor</p>
+                        <div class="circle">-2</div>
+                    </div>
+                    <div class="circles_numbers">
+                    <p>Floor</p>
+                        <div class="circle">-1</div>
+                    </div>
+                    <div class="circles_numbers">
+                    <p>Floor</p>
+                        <div class="circle">0</div>
+                    </div>
+                    <div class="circles_numbers">
+                    <p>Floor</p>
+                        <div class="circle">1</div>
+                    </div>
+                    <div class="circles_numbers">
+                    <p>Floor</p>
+                        <div class="circle">2</div>
+                    </div>
+                    <div class="circles_numbers">
+                    <p>Floor</p>
+                        <div class="circle">3</div>
+                    </div>
+                    <div class="circles_numbers">
+                    <p>Floor</p>
+                        <div class="circle">4</div>
+                    </div>
+                   
+                </div>
             </div>
             
         </section>
-        
-        `;
-    }
+    `;
+}
+
+mapTemplate();
+
+
