1af28320f9e156e38170fc1801d4a148687d9353
Wojtek <wdzwonek97@gmail.com>
2020-12-05 14:26:08 +0100

    navigation and home (not finished)

--- js/home.js
+++ js/home.js
@@ -1,23 +1,35 @@
+function appendHomePage(){
+    document.querySelector("#app").innerHTML += /*html*/`
+        <section id="home" class="page home-page">
+            <header class="topbar">
+                <h1>KuNSTE</h1>
+                <h2>Art museum</h2>
+            </header>
+            <div class="nav-btn" onclick="openCloseNav()">
+                <div></div>
+                <div></div>
+            </div>
+            <h3>SPA Template Modules Pages</h3>
+            <p>My Single Page Web App Template</p>
+        </section>
+    `;
+}   
 
+appendHomePage();
 
-    function templateTwo(){
-        document.querySelector("#app").innerHTML += /*html*/`
-            <section id="home" class="page home-page">
-                <header class="topbar">
-                    <h1>KuNSTE</h1>
-                    <h2>Art museum</h2>
-                </header>
-                <div class="nav-btn">
-                    <div></div>
-                    <div></div>
-                </div>
-                <h3>SPA Template Modules Pages</h3>
-                <p>My Single Page Web App Template</p>
-            </section>
-        `;
-        let navBtn = document.querySelector(".nav-btn");
-        navBtn.onclick = function(){
-            console.log("Opened nav");
-            document.querySelector(".nav-container").setAttribute("style", "right: 0px;");
-        }
-    }    
+function openCloseNav(){
+    appendNav();
+    let navContainer = document.querySelector(".nav-container");
+    if(navContainer.style.right = "-3000px"){
+        navContainer.style.right = "0px";
+        console.log("Open nav");
+    }   else if (navContainer.style.right = "0px"){
+        navContainer.style.right = "-3000px";
+        console.log("Close nav");
+    }
+    
+    
+
+}
+
+//document.querySelector(".nav-btn").addEventListener("click", openCloseNav);
