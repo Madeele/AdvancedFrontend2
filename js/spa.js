6a2345b0d676d76ccff41622ebe5333bccb5f4fb
Ana <eaaania@students.eaaa.dk>
2020-12-05 14:14:40 +0100

    spa changes

--- js/spa.js
+++ js/spa.js
@@ -1,29 +1,46 @@
 
-  
-  function init(){
-    this.page = document.querySelectorAll(".page");
-    this.pageChange();
-  }
+"use strict";
 
-  // Hide all pages
-  function hideAllPages() {
-    let pages = document.querySelectorAll(".page");
-    for (let page of pages) {
-      page.style.display = "none";
+// hide all pages
+function hideAllPages() {
+  let pages = document.querySelectorAll(".page");
+  for (let page of pages) {
+    page.style.display = "none";
+  }
+}
+
+// show page or tab
+function showPage(pageId) {
+  hideAllPages();
+  document.querySelector(`#${pageId}`).style.display = "block";
+  setActiveTab(pageId);
+}
+
+// sets active tabbar/ menu item
+function setActiveTab(pageId) {
+  let pages = document.querySelectorAll(".tabbar a");
+  for (let page of pages) {
+    if (`#${pageId}` === page.getAttribute("href")) {
+      page.classList.add("active");
+    } else {
+      page.classList.remove("active");
     }
   }
+}
 
-  // Show page or tab
-  function showPage(pageId) {
-    this.hideAllPages();
-    document.querySelector(`#${pageId}`).style.display = "block";
-  }
+// navigate to a new view/page by changing href
+function navigateTo(pageId) {
+  location.href = `#${pageId}`;
+}
 
-  //Change the page
-  function pageChange() {
-    let page = this.defaultPage;
-    if (window.location.hash) {
-      page = window.location.hash.slice(1);
-    }
-    this.showPage(page);
+// set default page or given page by the hash url
+// function is called 'onhashchange'
+function pageChange() {
+  let page = "home";
+  if (location.hash) {
+    page = location.hash.slice(1);
   }
+  showPage(page);
+}
+
+pageChange(); // called by default when the app is loaded for the first time
